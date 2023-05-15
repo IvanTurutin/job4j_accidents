3ef31e01b4adf243b733a3b870bddd28acd1c5da
Ivan <ivan.turutin@gmail.com>
2023-05-15 14:16:02 +0300

    initialization

--- src/main/java/ru/job4j/accidents/Main.java
+++ src/main/java/ru/job4j/accidents/Main.java
@@ -1,12 +1,7 @@
 package ru.job4j.accidents;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
@@ -15,11 +10,5 @@ public class Main {
         System.out.println("Go to http://localhost:8080/index");
     }
 
-    @Bean(destroyMethod = "close")
-    public SessionFactory sf() {
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure().build();
-        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
-    }
 }
 
