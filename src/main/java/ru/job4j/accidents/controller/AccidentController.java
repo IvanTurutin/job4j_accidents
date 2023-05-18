f36a408753d8b089c57a096bdc2a2e7b3458db3e
Ivan <ivan.turutin@gmail.com>
2023-05-18 16:24:34 +0300

    3.1. @RequestParam. Форма редактирования [#308708 #416968]

--- src/main/java/ru/job4j/accidents/controller/AccidentController.java
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.service.AccidentService;
+import ru.job4j.accidents.service.AccidentTypeService;
 
 import java.util.Optional;
 
@@ -21,6 +22,7 @@ import java.util.Optional;
 @Slf4j
 public class AccidentController {
     private final AccidentService accidentService;
+    private final AccidentTypeService accidentTypeService;
 
     /**
      * Подготавливает вид создания нового нарушения
@@ -30,7 +32,7 @@ public class AccidentController {
     @GetMapping("/createAccident")
     public String viewCreateAccident(Model model) {
         model.addAttribute("user", "Ivan");
-        model.addAttribute("accident", new Accident());
+        model.addAttribute("types", accidentTypeService.findAll());
         return "accident/createAccident";
     }
 
@@ -40,7 +42,7 @@ public class AccidentController {
      * @param accident формируемое нарушение
      * @return адрес шаблона начальной страницы
      */
-    @RequestMapping(value = "/saveAccident", method = RequestMethod.POST, params = "action=save")
+    @PostMapping("/saveAccident")
     public String save(Model model, @ModelAttribute Accident accident) {
         log.debug("accident at save() before add = " + accident);
         if (!accidentService.create(accident)) {
@@ -52,12 +54,31 @@ public class AccidentController {
     }
 
     /**
+     * Подготавливает вид для редактирования нарушения
+     * @param id идентификатор нарушения
+     * @param model модель вида
+     * @return адрес шаблона вида для редактирования нарушения
+     */
+    @GetMapping("/formUpdateAccident")
+    public String viewUpdate(@RequestParam("id") int id, Model model) {
+        Optional<Accident> accident = accidentService.findById(id);
+        if (accident.isEmpty()) {
+            model.addAttribute("message", "Не удалось найти нарушение.");
+            return "message/fail";
+        }
+        model.addAttribute("user", "Ivan");
+        model.addAttribute("types", accidentTypeService.findAll());
+        model.addAttribute("accident", accident.get());
+        return "accident/updateAccident";
+    }
+
+    /**
      * Обрабатывает запрос на обновление нарушения
      * @param model модель вида
      * @param accident формируемое нарушение
      * @return адрес шаблона начальной страницы
      */
-    @RequestMapping(value = "/saveAccident", method = RequestMethod.POST, params = "action=update")
+    @PostMapping("/updateAccident")
     public String update(Model model, @ModelAttribute Accident accident) {
         log.debug("accident at update() before update = " + accident);
         if (!accidentService.update(accident)) {
@@ -68,20 +89,4 @@ public class AccidentController {
         return "redirect:/index";
     }
 
-    /**
-     * Подготавливает вид для редактирования нарушения
-     * @param id идентификатор нарушения
-     * @param model модель вида
-     * @return адрес шаблона вида для редактирования нарушения
-     */
-    @GetMapping("/formUpdateAccident")
-    public String update(@RequestParam("id") int id, Model model) {
-        Optional<Accident> accident = accidentService.findById(id);
-        if (accident.isEmpty()) {
-            model.addAttribute("message", "Не удалось найти нарушение.");
-            return "message/fail";
-        }
-        model.addAttribute("accident", accident.get());
-        return "accident/createAccident";
-    }
 }
