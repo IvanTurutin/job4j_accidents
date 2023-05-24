3b49ea85da88786a2c8051cad44344e3051eb33f
Ivan <ivan.turutin@gmail.com>
2023-05-24 08:13:24 +0300

    4. Form с композиционным объектом [#305522 #417150]

--- src/main/java/ru/job4j/accidents/service/AccidentService.java
+++ src/main/java/ru/job4j/accidents/service/AccidentService.java
@@ -1,12 +1,15 @@
 package ru.job4j.accidents.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.model.AccidentType;
 import ru.job4j.accidents.repository.AccidentMem;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 /**
@@ -17,8 +20,10 @@ import java.util.Optional;
 @ThreadSafe
 @Service
 @AllArgsConstructor
+@Slf4j
 public class AccidentService {
     private final AccidentMem repository;
+    private final AccidentTypeService accidentTypeService;
 
     /**
      * Ищет все нарушения
@@ -34,7 +39,16 @@ public class AccidentService {
      * @return true если успешно добавлен, false если не добавлен.
      */
     public boolean create(Accident accident) {
-        return repository.add(accident).isPresent();
+        return repository.add(checkAccidentType(accident)).isPresent();
+    }
+
+    private Accident checkAccidentType(Accident accident) {
+        Optional<AccidentType> accidentType = accidentTypeService.findById(accident.getType().getId());
+        if (accidentType.isEmpty()) {
+            throw new NoSuchElementException("Такой тип нарушения не найден");
+        }
+        accident.setType(accidentType.get());
+        return accident;
     }
 
     /**
@@ -52,6 +66,6 @@ public class AccidentService {
      * @return true если успешно обновилось, false если не обновилось
      */
     public boolean update(Accident accident) {
-        return repository.update(accident);
+        return repository.update(checkAccidentType(accident));
     }
 }
