40ccaf895332a915d453d428dbdd4f71c9be8019
Ivan <ivan.turutin@gmail.com>
2023-07-06 20:21:51 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.accidents.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +19,7 @@ import ru.job4j.accidents.model.AccidentType;
 import ru.job4j.accidents.service.AccidentService;
 import ru.job4j.accidents.service.AccidentTypeService;
 import ru.job4j.accidents.service.RuleService;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
 import java.util.Optional;
@@ -80,5 +83,131 @@ class AccidentControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("message/fail"))
                 .andExpect(MockMvcResultMatchers.model().attribute("message", "Не удалось найти нарушение."));
+    }
+
+    @Test
+    @WithMockUser
+    void whenCreateSuccessfully() throws Exception {
+        Accident accident =
+                new Accident(
+                        0,
+                        "Accident1",
+                        "Accident text",
+                        "Accident address",
+                        new AccidentType(1, null),
+                        new HashSet<>());
+        String[] ids = {"1", "2"};
+        when(accidentService.create(accident, ids)).thenReturn(true);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/accidents/saveAccident")
+                        .param("id", String.valueOf(accident.getId()))
+                        .param("name", accident.getName())
+                        .param("text", accident.getText())
+                        .param("address", accident.getAddress())
+                        .param("type.id", String.valueOf(accident.getType().getId()))
+                        .param("rIds", ids))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/index"));
+
+        ArgumentCaptor<Accident> argument = ArgumentCaptor.forClass(Accident.class);
+        ArgumentCaptor<String[]> argument2 = ArgumentCaptor.forClass(String[].class);
+        Mockito.verify(accidentService).create(argument.capture(), argument2.capture());
+        assertThat(argument.getValue().getId()).isEqualTo(accident.getId());
+        assertThat(argument.getValue().getName()).isEqualTo(accident.getName());
+        assertThat(argument.getValue().getText()).isEqualTo(accident.getText());
+        assertThat(argument.getValue().getAddress()).isEqualTo(accident.getAddress());
+        assertThat(argument.getValue().getType().getId()).isEqualTo(accident.getType().getId());
+        assertThat(argument2.getValue()[0]).isEqualTo(ids[0]);
+        assertThat(argument2.getValue()[1]).isEqualTo(ids[1]);
+    }
+
+    @Test
+    @WithMockUser
+    void whenNotCreate() throws Exception {
+        Accident accident =
+                new Accident(
+                        0,
+                        "Accident1",
+                        "Accident text",
+                        "Accident address",
+                        new AccidentType(1, null),
+                        new HashSet<>());
+        String[] ids = {"1", "2"};
+        when(accidentService.create(accident, ids)).thenReturn(false);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/accidents/saveAccident")
+                        .param("id", String.valueOf(accident.getId()))
+                        .param("name", accident.getName())
+                        .param("text", accident.getText())
+                        .param("address", accident.getAddress())
+                        .param("type.id", String.valueOf(accident.getType().getId()))
+                        .param("rIds", ids))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("message/fail"))
+                .andExpect(MockMvcResultMatchers.model()
+                        .attribute("message", "Не удалось добавить новое нарушение."));
+    }
+
+    @Test
+    @WithMockUser
+    void whenUpdateSuccessfully() throws Exception {
+        Accident accident =
+                new Accident(
+                        1,
+                        "Accident1",
+                        "Accident text",
+                        "Accident address",
+                        new AccidentType(1, null),
+                        new HashSet<>());
+        String[] ids = {"1", "2"};
+        when(accidentService.update(accident, ids)).thenReturn(true);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/accidents/updateAccident")
+                        .param("id", String.valueOf(accident.getId()))
+                        .param("name", accident.getName())
+                        .param("text", accident.getText())
+                        .param("address", accident.getAddress())
+                        .param("type.id", String.valueOf(accident.getType().getId()))
+                        .param("rIds", ids))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/index"));
+
+        ArgumentCaptor<Accident> argument = ArgumentCaptor.forClass(Accident.class);
+        ArgumentCaptor<String[]> argument2 = ArgumentCaptor.forClass(String[].class);
+        Mockito.verify(accidentService).update(argument.capture(), argument2.capture());
+        assertThat(argument.getValue().getId()).isEqualTo(accident.getId());
+        assertThat(argument.getValue().getName()).isEqualTo(accident.getName());
+        assertThat(argument.getValue().getText()).isEqualTo(accident.getText());
+        assertThat(argument.getValue().getAddress()).isEqualTo(accident.getAddress());
+        assertThat(argument.getValue().getType().getId()).isEqualTo(accident.getType().getId());
+        assertThat(argument2.getValue()[0]).isEqualTo(ids[0]);
+        assertThat(argument2.getValue()[1]).isEqualTo(ids[1]);
+    }
+
+    @Test
+    @WithMockUser
+    void whenNotUpdate() throws Exception {
+        Accident accident =
+                new Accident(
+                        1,
+                        "Accident1",
+                        "Accident text",
+                        "Accident address",
+                        new AccidentType(1, null),
+                        new HashSet<>());
+        String[] ids = {"1", "2"};
+        when(accidentService.update(accident, ids)).thenReturn(false);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/accidents/updateAccident")
+                        .param("id", String.valueOf(accident.getId()))
+                        .param("name", accident.getName())
+                        .param("text", accident.getText())
+                        .param("address", accident.getAddress())
+                        .param("type.id", String.valueOf(accident.getType().getId()))
+                        .param("rIds", ids))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("message/fail"))
+                .andExpect(MockMvcResultMatchers.model()
+                        .attribute("message", "Не удалось обновить нарушение."));
     }
 }
--- src/test/java/ru/job4j/accidents/controller/RegControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/RegControllerTest.java
@@ -1,9 +1,13 @@
 package ru.job4j.accidents.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,11 +15,27 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.job4j.accidents.Main;
+import ru.job4j.accidents.model.Authority;
+import ru.job4j.accidents.model.User;
+import ru.job4j.accidents.service.AuthorityService;
+import ru.job4j.accidents.service.UserService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 class RegControllerTest {
+
+    @MockBean
+    private UserService users;
+
+    @MockBean
+    private PasswordEncoder encoder;
+
+    @MockBean
+    private AuthorityService authorities;
 
     @Autowired
     private MockMvc mockMvc;
@@ -27,5 +47,62 @@ class RegControllerTest {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("user/reg"));
+    }
+
+    @Test
+    @WithMockUser
+    void whenCreateSuccessfully() throws Exception {
+        User user = new User(
+                0,
+                "$2a$10$T0wIbKsejrnY9CLh3WQqlewbwMlFV7GyleMvXujKa1r8K6lXb5Aiq",
+                "User",
+                new Authority(1, "ROLE_USER"),
+                true);
+        String password = "123";
+        when(users.create(user)).thenReturn(true);
+        when(encoder.encode(password)).thenReturn(user.getPassword());
+        when(authorities.findByName(user.getAuthority().getAuthority())).thenReturn(user.getAuthority());
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/registration")
+                        .param("id", String.valueOf(user.getId()))
+                        .param("username", user.getUsername())
+                        .param("password", password))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/login"));
+
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        Mockito.verify(users).create(argument.capture());
+        assertThat(argument.getValue().getId()).isEqualTo(user.getId());
+        assertThat(argument.getValue().getUsername()).isEqualTo(user.getUsername());
+        assertThat(argument.getValue().getPassword()).isEqualTo(user.getPassword());
+        assertThat(argument.getValue().getAuthority()).isEqualTo(user.getAuthority());
+        assertThat(argument.getValue().isEnabled()).isEqualTo(user.isEnabled());
+    }
+
+    @Test
+    @WithMockUser
+    void whenNotCreate() throws Exception {
+        User user = new User(
+                0,
+                "$2a$10$T0wIbKsejrnY9CLh3WQqlewbwMlFV7GyleMvXujKa1r8K6lXb5Aiq",
+                "User",
+                new Authority(1, "ROLE_USER"),
+                true);
+        String password = "123";
+        when(users.create(user)).thenReturn(false);
+        when(encoder.encode(password)).thenReturn(user.getPassword());
+        when(authorities.findByName(user.getAuthority().getAuthority())).thenReturn(user.getAuthority());
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/registration")
+                        .param("id", String.valueOf(user.getId()))
+                        .param("username", user.getUsername())
+                        .param("password", password))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("/message/fail"))
+                .andExpect(MockMvcResultMatchers.model()
+                        .attribute("message", "Не удалось создать пользователя с таким именем. "
+                                + "Возможно имя уже занято."));
     }
 }
